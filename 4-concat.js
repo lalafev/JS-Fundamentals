ead708b0427707be85013d5422fed6f75c0531d2
lalafev <fevengebremichaelgit1@gmail.com>
2025-09-13 22:52:26 +0300

    Update 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -8,3 +8,10 @@ if (firstArgument === undefined) {
   // Otherwise, print the first argument.
   console.log(firstArgument);
 }
+guillaume@ubuntu:~/0x12$ node 4-concat.js c cool
+c is cool
+guillaume@ubuntu:~/0x12$ node 4-concat.js c
+c is undefined
+guillaume@ubuntu:~/0x12$ node 4-concat.js
+undefined is undefined
+guillaume@ubuntu:~/0x12$ 
